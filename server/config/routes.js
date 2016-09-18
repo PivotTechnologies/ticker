0216f0ce4f39d75d8f0a39178e05f4f62560685b
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-09-17 17:09:04 -0700

    [setup] - initializes backend file structure

---
+++ server/config/routes.js
@@ -0,0 +1,21 @@
+// endpoints
+
+
+/* user actions */
+// /signup
+// /signin
+// /getProfile -> return active or past auctions, buy or sell
+
+/* event actions */
+// /buyerSearch
+  // takes: query(event name, category, venue), location or date
+  // returns: array of matching event objects
+
+/* auction actions */
+// /createAuction -> also creates the event if not there, updates event_tickets_available
+// /sellerSearch
+// /cancelAuction -> removes the auction if seller cancels or auction expires
+// /getAuctions
+  // takes: eventId
+  // returns: array of auction objects
+// /buyTickets -> will de-list auction

141ed9241036e15e8ce8a018c552445334cca3b3
Eugene Row <eugene.row@gmail.com>
2016-09-19 19:35:39 -0700

    [styles] - imports flexboxgrid

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-// import { Router, Route, IndexRoute, hashHistory, browserHistory } from 'react-router';
+//import { Router, Route, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
 ReactDOM.render(<App />, document.getElementById('app'));

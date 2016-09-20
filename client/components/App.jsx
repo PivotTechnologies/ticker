81d1710d65434405aa6449e0aecb9283dc63c54c
Carling Sugarman <csugarman@hmc.edu>
2016-09-20 16:10:48 -0700

    [feature] - creates links in navbar (non-functional)

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import NavBar from './NavBar.jsx';
 // import Signup from './generalUser/Signup.jsx';
 // import Signin from './generalUser/Signin.jsx';
-// import BuyerView from './buyer/BuyerView.jsx';
-// import EventView from './buyer/EventView.jsx';
-// import AuctionView from './buyer/AuctionView.jsx';
-import UserProfile from './generalUser/UserProfile.jsx';
+import BuyerView from './buyer/BuyerView.jsx';
+import EventView from './buyer/EventView.jsx';
+import AuctionView from './buyer/AuctionView.jsx';
+// import UserProfile from './generalUser/UserProfile.jsx';
 
 const App = () => (
   <div>
     <NavBar />
-    <UserProfile />
+    <BuyerView />
+    <EventView />
+    <AuctionView />
   </div>
 );
 

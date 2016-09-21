10cd0c3c36d89174d1c6284d5b03f6afd2c8aeb6
Carling Sugarman <csugarman@hmc.edu>
2016-09-21 10:34:17 -0700

    [fix] - fixes error in user model

--- client/reducers/reducer_user.js
+++ client/reducers/reducer_user.js
@@ -4,6 +4,7 @@ import { SIGNIN, SIGNUP } from '../actions/index';
 export default function (state = {}, action) {
   switch (action.type) {
     case SIGNIN:
+      console.log(action.payload.data)
       return action.payload; // change when we add an axios request
 
     case SIGNUP:

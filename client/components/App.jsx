199c862566bcecddd01aa9ae3749a6cf35df67ac
Carling Sugarman <csugarman@hmc.edu>
2016-09-20 19:45:57 -0700

    [setup] - sets up react router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import NavBar from './NavBar.jsx';
-// import Signup from './generalUser/Signup.jsx';
-// import Signin from './generalUser/Signin.jsx';
-import BuyerView from './buyer/BuyerView.jsx';
-import EventView from './buyer/EventView.jsx';
-import AuctionView from './buyer/AuctionView.jsx';
-// import UserProfile from './generalUser/UserProfile.jsx';
+// // import Signup from './generalUser/Signup.jsx';
+// // import Signin from './generalUser/Signin.jsx';
+// import BuyerView from './buyer/BuyerView.jsx';
+// import EventView from './buyer/EventView.jsx';
+// import AuctionView from './buyer/AuctionView.jsx';
+// // import UserProfile from './generalUser/UserProfile.jsx';
 
-const App = () => (
+const App = ({ children }) => (
   <div>
     <NavBar />
-    <BuyerView />
-    <EventView />
-    <AuctionView />
+    {children}
   </div>
 );
 

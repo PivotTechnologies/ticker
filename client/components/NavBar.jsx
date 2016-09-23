40c46b2c3744e17c54af64373b3d0bdf3ac2cd5d
Carling Sugarman <csugarman@hmc.edu>
2016-09-23 09:25:05 -0700

    [style] - adds material-ui appbar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar';
 
 const NavBar = () => (
-  <nav>
-    <a href="/"><h1>ticker</h1></a>
-    <a href="/signup">Sign Up</a>
-    <a href="/signin">Sign In</a>
-    <a href="/account">My Account</a>
-    <a href="/">Find Tickets</a>
-    <a href="/sell">Sell Tickets</a>
-  </nav>
+  <AppBar
+    title="ticker"
+  />
 );
 
 export default NavBar;
+
+// <a href="/"><h1>ticker</h1></a>
+// <a href="/signup">Sign Up</a>
+// <a href="/signin">Sign In</a>
+// <a href="/account">My Account</a>
+// <a href="/">Find Tickets</a>
+// <a href="/sell">Sell Tickets</a>

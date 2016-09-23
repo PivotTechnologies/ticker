0d4a105d809f5353015930c3a827cca7ebf81cf1
Carling Sugarman <csugarman@hmc.edu>
2016-09-23 14:33:35 -0700

    [style] - starts to create menu on navbar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,11 +1,55 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FaceIcon from 'material-ui/svg-icons/action/face';
+import FlatButton from 'material-ui/FlatButton';
 
-const NavBar = () => (
-  <AppBar
-    title="ticker"
-  />
-);
+class NavBar extends React.Component {
+  renderRightElement() {
+    if (true){//(localStorage.getItem('userToken')) { // IF LOGGED IN, SHOW ACCOUNT STUFF
+      return (
+        <IconMenu
+          iconButtonElement={
+            <IconButton><FaceIcon /></IconButton>
+          }
+          targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+        >
+          <MenuItem primaryText="My Account" />
+          <MenuItem primaryText="Sign Out" />
+        </IconMenu>
+      );
+    }
+    // IF NOT LOGGED IN, SHOW SIGNIN/SIGNUP BUTTON
+    return (
+      <FlatButton label="Sign In / Sign Up" />
+    );
+  }
+
+  render() {
+    return (
+      <AppBar
+        title="ticker"
+        iconElementLeft={
+          <IconMenu
+            iconButtonElement={
+              <IconButton><FaceIcon /></IconButton>
+            }
+            targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+          >
+            <MenuItem primaryText="My Account" />
+            <MenuItem primaryText="Sign Out" />
+          </IconMenu>
+        }
+        iconElementRight={this.renderRightElement()}
+      />
+    );
+  }
+
+}
 
 export default NavBar;
 

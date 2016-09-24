945a09b5b28cadf86c1044a978d337fdbb401105
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-09-23 18:19:49 -0700

    [fix] - updates controllers to reflect database refactoring

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,23 +1,3 @@
-// endpoints
-/* user actions */
-// /signup
-// /signin
-// /getProfile -> return active or past auctions, buy or sell
-
-/* event actions */
-// /buyerSearch
-  // takes: query(event name, category, venue), location or date
-  // returns: array of matching event objects
-
-/* auction actions */
-// /createAuction -> also creates the event if not there, updates event_tickets_available
-// /sellerSearch
-// /cancelAuction -> removes the auction if seller cancels or auction expires
-// /getAuctions
-  // takes: eventId
-  // returns: array of auction objects
-// /buyTickets -> will de-list auction
-
 const userController = require('../controllers/userController');
 const eventController = require('../controllers/eventController');
 const auctionController = require('../controllers/auctionController');

7ed98b6feebd77af1d7bccad45263e3ef4458b15
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-09-23 18:36:59 -0700

    [feature] - implements create auction

--- server/controllers/auctionController.js
+++ server/controllers/auctionController.js
@@ -2,37 +2,67 @@ const models = require('../models/models');
 
 module.exports = {
 
-    create: (req, res) => {
-      models.Event.findOne({
-          where: {
-            id: req.body.eventId
-          }
-        })
-        .then( event => {
-          if(!event){
-            // NEED TO CREATE NEW EVENT IF WE DON'T HAVE IT ALREADY
-            // ALSO NEED TO DO EVERYTHING IN THE ELSE (CREATE THE AUCTION)
-            res.send("Invalid eventId.")
-          }
-          else {
-            const newAuction = models.Auction.create({
-              sellerId: req.body.sellerId,
-              buyerId: '',
-              eventId: req.body.eventId,
-              startPrice: req.body.startPrice,
-              currentPrice: req.body.startPrice,
-              minPrice: req.body.minPrice,
-              numTickets: req.body.numTickets,
-              status: 'On Sale',
-            })
-            .then( (auction) => {
-              console.log('New auction created: ', auction.dataValues);
-              res.send(auction.dataValues);
-            })
-            .catch( err => console.log('Error:', err) );
-          }
-        })
-        .catch( err => console.log('Error:', err) );
+  create: (req, res) => {
+    /* TODO: test to make sure all info is coming it */
+    let auctionEventId;
+    /* create auction model, doesn't save to db yet*/
+    const newAuction = models.Auction.build({
+      eventId: '',
+      sellerId: req.body.userId,
+      startPrice: req.body.startPrice,
+      minPrice: req.body.minPrice,
+      numTickets: req.body.numTickets,
+      status: 'On Sale',
+    })
+    /* look for event passed in */
+    models.Event
+      .findOne({
+        where: {
+          name: {
+            $iLike: req.body.event.name
+          },
+          venue: {
+            $iLike: req.body.event.venue
+          },
+          // date: {
+          //   $eq: req.body.event.datetime_local
+          // },
+        }
+      })
+      .then( event => {
+        /* no event found, create it */
+        if(!event){
+          newEvent = models.Event.create({
+            // update schema
+            name: req.body.event.name,
+            venue: req.body.event.venue,
+            city: req.body.event.city,
+            time: req.body.event.time,
+            category: req.body.event.category,
+            date: req.body.event.date
+          })
+          .then( (event) => {
+            console.log('New event created: ', event.dataValues);
+            auctionEventId = event.dataValues.id;
+          })
+          .catch( err => console.log('Error:', err) );
+        }
+        /* event found */
+        else {
+          console.log('Event found: ', event.dataValues);
+          auctionEventId = event.dataValues.id;
+        }
+        /* update our auction with event details */
+        newAuction.eventId = auctionEventId;
+        newAuction
+          .save()
+          .then( auction => {
+            console.log('New auction created: ', auction.dataValues);
+            res.send(auction.dataValues);
+          })
+          .catch( err => console.log('Error:', err) )
+      })
+      .catch( err => console.log('Error:', err) );
     },
 
     cancel: (req, res) => {

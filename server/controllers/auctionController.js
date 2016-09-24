5ce8a836935c033dc52a1c0ef6932195fe343666
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-09-24 10:57:59 -0700

    [fix] - fixes date on event models, auction & controllers

--- server/controllers/auctionController.js
+++ server/controllers/auctionController.js
@@ -4,16 +4,26 @@ module.exports = {
 
   create: (req, res) => {
     /* TODO: test to make sure all info is coming it */
+    function createAuction(){
+      const newAuction = models.Auction.build({
+        eventId: auctionEventId,
+        sellerId: req.body.userId,
+        startPrice: req.body.startPrice,
+        minPrice: req.body.minPrice,
+        numTickets: req.body.numTickets,
+        status: 'On Sale',
+        eventName: req.body.event.name,
+        eventDate: req.body.event.datetime_local
+      })
+      newAuction
+        .save()
+        .then( auction => {
+          console.log('New auction created: ', auction.dataValues);
+          res.send(auction.dataValues);
+        })
+        .catch( err => console.log('Error:', err) )
+    }
     let auctionEventId;
-    /* create auction model, doesn't save to db yet*/
-    const newAuction = models.Auction.build({
-      eventId: '',
-      sellerId: req.body.userId,
-      startPrice: req.body.startPrice,
-      minPrice: req.body.minPrice,
-      numTickets: req.body.numTickets,
-      status: 'On Sale',
-    })
     /* look for event passed in */
     models.Event
       .findOne({
@@ -24,43 +34,40 @@ module.exports = {
           venue: {
             $iLike: req.body.event.venue
           },
-          // date: {
-          //   $eq: req.body.event.datetime_local
-          // },
+          datetime_local: {
+            $lte: req.body.event.datetime_local
+          },
         }
       })
       .then( event => {
         /* no event found, create it */
         if(!event){
           newEvent = models.Event.create({
-            // update schema
             name: req.body.event.name,
+            address: req.body.event.address,
+            zip: req.body.event.zip,
+            state: req.body.event.state,
+            category: req.body.event.category,
+            image: req.body.event.image,
             venue: req.body.event.venue,
             city: req.body.event.city,
-            time: req.body.event.time,
             category: req.body.event.category,
-            date: req.body.event.date
+            datetime_local: req.body.event.datetime_local, // need to convert to pg format
+            timezone: req.body.event.timezone,
+            latitude: req.body.event.latitude,
+            longitude: req.body.event.longitude
           })
           .then( (event) => {
             console.log('New event created: ', event.dataValues);
-            auctionEventId = event.dataValues.id;
+            createAuction();
           })
           .catch( err => console.log('Error:', err) );
         }
         /* event found */
         else {
           console.log('Event found: ', event.dataValues);
-          auctionEventId = event.dataValues.id;
+          createAuction();
         }
-        /* update our auction with event details */
-        newAuction.eventId = auctionEventId;
-        newAuction
-          .save()
-          .then( auction => {
-            console.log('New auction created: ', auction.dataValues);
-            res.send(auction.dataValues);
-          })
-          .catch( err => console.log('Error:', err) )
       })
       .catch( err => console.log('Error:', err) );
     },
--- server/databaseTest.js
+++ server/databaseTest.js
@@ -1,25 +1,34 @@
-const Event = require('./models/eventModel');
-const Auction = require('./models/auctionModel');
-//
-// var testAuction = Auction.create({
-//   sellerId: '1',
-//   eventId: '99',
-//   startPrice: '100',
-//   minPrice: '50',
-//   numTickets: '1',
-// });
+const models = require('./models/models.js');
 
-Event.findOne({
-    where: {
-      id: 2
-    }
-  })
-  .then( auction => {
-    console.log(auction);
-    auction.updateAttributes({
-      eventId: '1'
-    })
-  })
-  .catch( err => {
-    console.log('Error:', err);
-  });
+// newEvent = models.Event.create({
+//   // update schema
+//   name: 'req.body.event.name2',
+//   address: 'req.body.event.address',
+//   zip: 'req.body.event.zip',
+//   state: 'req.body.event.state',
+//   category: 'req.body.event.category',
+//   image: 'req.body.event.image',
+//   venue: 'req.body.event.venue',
+//   city: 'req.body.event.city',
+//   category: 'req.body.event.category',
+//   datetime_local: '2016-09-24T21:00:00',
+//   timezone: 'req.body.event.timezone',
+//   latitude: 'req.body.event.latitude',
+//   longitude: 'req.body.event.longitude'
+// })
+// .then( event => console.log(event.dataValues) )
+
+newAuction = models.Auction.create({
+  sellerId: '1',
+  buyerId: '1',
+  eventId: '1',
+  startPrice: '1',
+  currentPrice: '1',
+  minPrice: '1',
+  numTickets: '1',
+  sellDate: '',
+  status: '',
+  eventName: '',
+  eventDate: '2016-09-24T21:00:00'
+})
+.then( auction => console.log(auction.dataValues) )

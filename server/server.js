d15ad38ce6e07afe57d94b721b4dedc28ba85be4
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-09-30 12:03:38 -0700

    [feature] - implements basic auction price update schedule

--- server/server.js
+++ server/server.js
@@ -1,8 +1,41 @@
 const express = require('express');
+const cluster = require('cluster');
+const schedule = require('node-schedule');
+const models = require('./models/models');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 require('./config/middleware')(app, express);
 require('./config/routes')(app, express);
+//
+// const test = schedule.scheduleJob('*/5 * * * * *', () => {
+//   const now = new Date();
+//   console.log('Scheduling test @ ', now.toTimeString() );
+// });
+
+// */5 * * * * * = every five seconds
+// * /5 * * * * = every five minute
+
+const decrementPrice = schedule.scheduleJob('*/5 * * * * *', () => {
+  const list = [];
+  models.Auction
+    .findAll({
+      where: {
+        status: 'On Sale'
+      },
+      // attributes: ['id', 'eventName', 'eventDate', 'startPrice', 'currentPrice', 'minPrice', 'numTickets', 'status']
+      attributes: ['id', 'eventName']
+    })
+    .then( auctions => {
+      auctions.forEach( auction => {
+        //console.log('auction:', auction.dataValues);
+        list.push(auction.dataValues)
+      });
+      console.log('Updating the following auctions: ', list);
+    })
+
+
+});
 
 app.listen(port, () => console.log('\033[34m🎟  Ticker server listening on port: \033[0m', port) );

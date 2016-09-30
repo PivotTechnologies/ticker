beb8c3ff04922034610da21d1b789b0764142024
Carling Sugarman <csugarman@hmc.edu>
2016-09-30 12:25:57 -0700

    [feature] - sets up constant fetching of auctions to render price updates on auction list

--- client/components/buyer/AuctionList.jsx
+++ client/components/buyer/AuctionList.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchAuctions } from '../../actions/index.js';
+import { bindActionCreators } from 'redux';
 import AuctionListItem from './AuctionListItem.jsx';
 
 class AuctionList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      intervalId: null
+    };
+  }
+
+  componentDidMount() {
+    const id = setInterval(() => {
+      console.log('updating auctions!')
+      this.props.fetchAuctions(this.props.activeEvent.id);
+    }, 1000);
+    this.setState({ intervalId: id });
+  }
+
+  componentWillUnmount() {
+    console.log('no longer updating auctions')
+    clearInterval(this.state.intervalId);
+  }
+
   renderAuctionList() {
     return this.props.auctions.map((auction, idx) =>
       <AuctionListItem key={idx} auction={auction} />
@@ -20,8 +43,13 @@ class AuctionList extends React.Component {
 
 function mapStateToProps(state) {
   return {
+    activeEvent: state.activeEvent,
     auctions: state.auctions,
   };
 }
 
-export default connect(mapStateToProps)(AuctionList);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchAuctions }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuctionList);

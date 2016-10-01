f6f58a427d03086938d1e35bc3076286302f7b8b
Carling Sugarman <csugarman@hmc.edu>
2016-10-01 12:16:35 -0700

    [style] - styles event list items

--- client/components/buyer/EventView.jsx
+++ client/components/buyer/EventView.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
 import EventDetails from './EventDetails.jsx';
 import AuctionList from './AuctionList.jsx';
 
 const EventView = () => (
-    <div>
-        <EventDetails/>
-        <AuctionList/>
+  <Paper zDepth={0}>
+    <EventDetails />
+    <div className="auction-list">
+      <AuctionList />
     </div>
+  </Paper>
 );
 
 export default EventView;

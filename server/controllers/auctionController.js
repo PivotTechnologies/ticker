e23c6f8c9f360d9469525627f1310909b3986d46
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-10-03 12:02:08 -0700

    [fix] - fixed missing auctionController code

--- server/controllers/auctionController.js
+++ server/controllers/auctionController.js
@@ -190,6 +190,38 @@ module.exports = {
     .catch( err => console.log(err) )
   },
 
+  fetchById: (req, res) => {
+    models.Auction
+      .findOne({
+        where: {
+          id: req.query.auctionId
+        },
+        attributes: [
+          'id',
+          'sellerId',
+          'buyerId',
+          'eventId',
+          'startPrice',
+          'currentPrice',
+          'minPrice',
+          'numTickets',
+          'sellDate',
+          'status',
+          'eventName',
+          'eventDate'
+        ]
+      })
+      .then( auction => {
+        if(auction) {
+          res.json(auction.dataValues);
+        }
+        else {
+          res.send("Auction not found.");
+        }
+      })
+      .catch( err => console.log(err) )
+  },
+  
   buyTickets: (req, res) => {
     models.Auction
       .findOne({

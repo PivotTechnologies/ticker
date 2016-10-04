46b305911b10a57e64fbe07124a13c7a0559d863
Carling Sugarman <csugarman@hmc.edu>
2016-10-03 19:31:50 -0700

    [feature] - expires events once event time has passed, standardizes time in db to PDT

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cluster = require('cluster');
 const schedule = require('node-schedule');
+const moment = require('moment-timezone');
 const models = require('./models/models');
 const pricing = require('./config/pricingHelper');
 const app = express();
@@ -10,27 +11,59 @@ require('./config/middleware')(app, express);
 require('./config/routes')(app, express);
 
 const decrementPrice = schedule.scheduleJob('*/5 * * * * *', () => {
-  const list = [];
-  models.Auction
-    .findAll({
-      where: {
-        status: 'On Sale'
-      },
-      attributes: [
-        'id', 'eventName', 'eventDate', 'minPrice',
-        'startTime', 'startPrice',
-        'coefA', 'coefB'
-      ]
-    })
-    .then( auctions => {
-      auctions.forEach( auction => {
-        auction.currentPrice = pricing.calculatePrice( auction.dataValues.startTime,
-                                                       auction.dataValues.startPrice,
-                                                       auction.dataValues.coefA,
-                                                       auction.dataValues.coefB );
-        auction.save();
-      });
-    })
+  models.Auction.findAll({
+    where: {
+      status: 'On Sale'
+    },
+    attributes: [
+      'id', 'eventName', 'eventDate', 'minPrice',
+      'startTime', 'startPrice',
+      'coefA', 'coefB'
+    ]
+  })
+  .then( auctions => {
+    auctions.forEach( auction => {
+      auction.currentPrice = pricing.calculatePrice( auction.dataValues.startTime,
+                                                     auction.dataValues.startPrice,
+                                                     auction.dataValues.coefA,
+                                                     auction.dataValues.coefB );
+      auction.save();
+    });
+  })
+});
+//0 * * * * -> every hour
+const expireAuctions = schedule.scheduleJob('*/5 * * * * *', () => {
+  const now = new Date();
+  const nowLA = new Date(moment.tz(now, 'America/Los_Angeles').format('YYYY-MM-DDTHH:mm:ss'));
+  console.log("IT IS NOW:", nowLA)
+
+  models.Event.findAll({
+    where: {
+      eventDate: {
+        $lte: nowLA
+      }
+    },
+    attributes: ['id', 'numAuctions']
+  })
+  .then( events => {
+    events.forEach( event => {
+      models.Auction.findAll({
+        where: {
+          eventId: event.id,
+          status: 'On Sale'
+        },
+        attributes: ['id', 'status']
+      })
+      .then( auctions => {
+        auctions.forEach( auction => {
+          auction.status = 'Expired';
+          auction.save();
+        })
+      })
+      event.numAuctions = 0;
+      event.save();
+    });
+  })
 });
 
 app.listen(port, () => console.log('\033[34m🎟  Ticker server listening on port: \033[0m', port) );

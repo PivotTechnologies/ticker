6624dadfab93846accebcd5e9c5794647af0aba2
Eugene Row <eugene.row@gmail.com>
2016-10-04 11:19:59 -0700

    [fix] - fixes datetimelocal error

--- client/components/buyer/BuyerView.jsx
+++ client/components/buyer/BuyerView.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import BuyerSearchBar from './BuyerSearchBar.jsx';
 import EventList from './EventList.jsx';
-import Maps from '../Maps.jsx';
 
 const BuyerView = () => (
   <Paper className="main-content" zDepth={0}>
     <h1>Find Tickets</h1>
     <BuyerSearchBar />
     <EventList />
-   <Maps /> 
-
   </Paper>
 );
 

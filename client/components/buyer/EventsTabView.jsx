9c891109fd9b2ef8838318e8d814beb989343466
Eugene Row <eugene.row@gmail.com>
2016-10-06 18:32:12 -0700

    [fix] - fixes conflicts in App.jsx

--- client/components/buyer/EventsTabView.jsx
+++ client/components/buyer/EventsTabView.jsx
@@ -4,7 +4,6 @@ import EventList from './EventList.jsx';
 
 class EventsTabView extends React.Component {
   render() {
-    console.log("this.props.events in events tab view:", this.props.events);
     if (this.props.events && !this.props.events.length) {
       return (<div></div>);
     } else {

45af1d4d7f536273c9589c3747712cc61c31332c
Eugene Row <eugene.row@gmail.com>
2016-10-10 08:25:36 -0700

    [fix] - displays images for events without pictures in event details view

--- client/components/buyer/EventDetails.jsx
+++ client/components/buyer/EventDetails.jsx
@@ -3,58 +3,87 @@ import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'm
 import { connect } from 'react-redux';
 import moment from 'moment';
 
-const EventDetails = ({ activeEvent }) => {
-  return (
-    <div className="event-details">
-      <Card className="event-date" style={{height: '210px', width: '210px', background: '#2a2c43', color: '#f2b632', borderRadius: '8px 0px 0px 8px'}}>
-        <div className="event-details-month">
-          {moment(activeEvent.eventDate).format('MMM').toUpperCase()}
-        </div>
-        <div className="event-details-day">
-          {moment(activeEvent.eventDate).format('DD').toUpperCase()}
-        </div>
-        <div className="event-details-weekday">
-          {moment(activeEvent.eventDate).format('ddd').toUpperCase()}
-        </div>
-      </Card>
-      <Card style={{flex: '1', height: '210px'}}>
-        <CardTitle
-          title={activeEvent.name}
-          subtitle={
-            <div className="event-details-text">
-              <div>
+ class EventDetails extends React.Component {
+   constructor (props) {
+     super(props)
+
+     this.renderEventImage = this.renderEventImage.bind(this);
+   }
+
+
+
+  renderEventImage(event) {
+    console.log("event images:", event.image);
+    if(event.image) {
+      return event.image;
+    }
+      switch(event.category) {
+        case 'sports':
+          return '../../assets/images/sportsEvent.png';
+
+        case 'concert':
+          return '../../assets/images/musicEvent.png';
+
+        case 'theater':
+          return '../../assets/images/theaterEvent.png';
+      }
+  };
+
+  render() {
+    const activeEvent = this.props.activeEvent;
+
+    return (
+      <div className="event-details">
+        <Card className="event-date" style={{height: '210px', width: '210px', background: '#2a2c43', color: '#f2b632', borderRadius: '8px 0px 0px 8px'}}>
+          <div className="event-details-month">
+            {moment(activeEvent.eventDate).format('MMM').toUpperCase()}
+          </div>
+          <div className="event-details-day">
+            {moment(activeEvent.eventDate).format('DD').toUpperCase()}
+          </div>
+          <div className="event-details-weekday">
+            {moment(activeEvent.eventDate).format('ddd').toUpperCase()}
+          </div>
+        </Card>
+        <Card style={{flex: '1', height: '210px'}}>
+          <CardTitle
+            title={activeEvent.name}
+            subtitle={
+              <div className="event-details-text">
+                <div>
+                  <br />
+                  <div>
+                    {activeEvent.venue}
+                  </div>
+                  <div>
+                    {activeEvent.address}
+                  </div>
+                  <div>
+                    {activeEvent.city}, {activeEvent.state} {activeEvent.zip}
+                  </div>
+                </div>
                 <br />
                 <div>
-                  {activeEvent.venue}
-                </div>
-                <div>
-                  {activeEvent.address}
-                </div>
-                <div>
-                  {activeEvent.city}, {activeEvent.state} {activeEvent.zip}
-                </div>
-              </div>
-              <br />
-              <div>
-                <div>
-                  {moment(activeEvent.eventDate).format('MMMM Do, YYYY')}
-                </div>
-                <div>
-                  {moment(activeEvent.eventDate).format('h:mma')}
+                  <div>
+                    {moment(activeEvent.eventDate).format('MMMM Do, YYYY')}
+                  </div>
+                  <div>
+                    {moment(activeEvent.eventDate).format('h:mma')}
+                  </div>
                 </div>
               </div>
-            </div>
-          }
-        />
-      </Card>
-      <Card style={{ borderRadius: '0px 8px 8px 0px' }}>
-        <CardMedia>
-          <img className="event-list-item-image" src={activeEvent.image} />
-        </CardMedia>
-      </Card>
-    </div>
-  );
-};
+            }
+          />
+        </Card>
+        <Card style={{ borderRadius: '0px 8px 8px 0px' }}>
+          <CardMedia>
+            <img className="event-list-item-image" src={this.renderEventImage(activeEvent)} />
+          </CardMedia>
+        </Card>
+      </div>
+    );
+  }
+}
 
 function mapStateToProps(state) {
   return {
